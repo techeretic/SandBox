a246684a474f7a026af9dd3c462366ce42fcedd3
Prathamesh Shetye <p.shetye@sta.samsung.com>
2015-04-29 18:01:49 -0500

    Fix that Scheduler

--- src/SequentialScheduler.java
+++ src/SequentialScheduler.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.LinkedBlockingQueue;
+
 
 
 //ThreadPool has a non-blocking post method that returns without blocking, 
@@ -6,12 +8,54 @@
 //Implement a class SequentialExecutor that wraps ThreadPool, and also has a non-blocking post method, but 
 //guarantees that the Runnables will be run sequentially, in the order they are given.
 public class SequentialScheduler {
+	ThreadPool threadPool;
+	LinkedBlockingQueue<Runnable> myQueue = new LinkedBlockingQueue<Runnable>();
+	private static Worker sWorker;
+	
+	public void addTask(Runnable r) {
+		myQueue.add(r);
+	}
+	
+	public void post() {
+		if (worker == null) {
+			worker = new Worker
+		}
+	}
 
+	private static Integer takeFromQueue() {
+        if (workerQueue == null) {
+            return null;
+        }
+        try {
+            return workerQueue.take();
+        } catch (InterruptedException e) {
+            Log.e(LOG_TAG, "workerQueue Exception", e);
+            return null;
+        }
+    }
+	
+	class ThreadPool {
+		public void post(Runnable r) {
+			new Thread(r).start();
+		}
+	}
+
+	class Worker extends Thread {
+		Runnable mRun;
+		
+		public Worker() {
+			super("SequentialScheduler");
+		}
+
+		@Override
+		public void run() {
+            super.run();
+            int count = takeFromQueue();
+		}
+	}
 }
 /*
-class ThreadPool {
-public void post(Runnable r);
-}
+
 
 
 class SequentialExecutor {
